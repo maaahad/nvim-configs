03c049b0e48f608d9437b7291a927d1a1f0eca9b
Muhammed Ahad <maaahad@gmail.com>
2024-01-27 16:33:42 +0100

    add keymap for ESC : jk in mode n and v

--- lua/plugins/darcula-solid.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "briones-gabriel/darcula-solid.nvim",
-  dependencies = { "rktjmp/lush.nvim" },
-  priority = 1000,
-  config = function()
-    vim.cmd([[colorscheme darcula-solid]])
-  end,
-}
--- lua/plugins/kanagawa.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "rebelot/kanagawa.nvim",
-  priority = 1000,
-  config = function()
-    -- load the colorscheme here
-    vim.cmd([[colorscheme tokyonight]])
-  end,
-}
