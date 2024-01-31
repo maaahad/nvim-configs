22ccb0095fa56821507f73306bcf5c3677bf947d
Muhammed Ahad <maaahad@gmail.com>
2024-01-31 22:57:52 +0100

    keymap added for moveline

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,11 +3,10 @@
 -- Exit insert mode with jk
 -- Add any additional keymaps here
 vim.keymap.set("i", "jk", "<ESC>", { noremap = true, silent = true, desc = "<ESC>" })
-
 -- Move Lines
-vim.keymap.set("n", "<leader>mj", "<cmd>m .+1<cr>==", { desc = "Move down" })
-vim.keymap.set("n", "<leader>mk", "<cmd>m .-2<cr>==", { desc = "Move up" })
-vim.keymap.set("i", "<leader>mj", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
-vim.keymap.set("i", "<leader>mk", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
-vim.keymap.set("v", "<leader>mj", ":m '>+1<cr>gv=gv", { desc = "Move down" })
-vim.keymap.set("v", "<leader>mk", ":m '<-2<cr>gv=gv", { desc = "Move up" })
+vim.keymap.set("n", "<C-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+vim.keymap.set("n", "<C-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+vim.keymap.set("i", "<C-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+vim.keymap.set("i", "<C-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+vim.keymap.set("v", "<C-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+vim.keymap.set("v", "<C-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
